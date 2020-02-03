e24fc74f784980c2609020310faeb23422d304c5
shyd82 <shyd82@hotmail.com>
2020-02-03 13:49:27 -0500

    initial change

---
+++ app.js
@@ -0,0 +1,39 @@
+// alert('Hello World 2020')
+
+// const name = 'Tom John';
+// console.log(name);
+
+// console.log(typeof name);
+
+
+// const name = 'Mick';
+// const job = 'junior DBA';
+// const today = new Date();
+
+
+
+// console.log(today);
+// console.log(job);
+// console.log(typeof today);
+
+const number1 = 50;
+const number2 = 100;
+let val;
+val = number1 + number2;
+val = number1 * number2;
+val = number1 % number2;
+
+
+val = Math.round(2.7);
+val = Math.ceil(6.3);
+val = Math.floor(6.9);
+val = Math.sqrt(25);
+val = Math.pow(5,3);
+
+val = Math.random();
+
+val = Math.random()*20;
+
+val =Math.round(Math.random()*30);
+
+console.log(val);
