064be4960ed8ffec3e4c2e21078b6d193e47b613
shyd82 <shyd82@hotmail.com>
2020-02-07 09:58:33 -0500

    lesson-17 switcher

---
+++ app3.js
@@ -0,0 +1,52 @@
+// contax:
+// if (something){
+//     do something
+// } else {
+//     do something else
+// }
+
+
+const id = 100;
+
+
+// it doesn't matter string or number
+// you can use 
+// const id = "100"
+
+// equal to
+
+if (id==1002){
+    console.log('Correct')
+} else {
+    console.log('Incorrect')
+}
+
+// not equal to
+if (id != 1002){
+    console.log('Correct')
+} else {
+    console.log('Incorrect')
+}
+
+
+
+const color = 'yellow';
+
+if (color === 'red'){
+    console.log('color is Red');
+} 
+else if(color ==='blue'){
+    console.log('color is Blue');
+}
+else {
+    console.log('color is not Red or Blue');
+}
+
+
+// 简单写法：
+
+console.log(id == 100 ? 'correct' : 'incorrect');
+
+// Udemy lesson-17 switcher
+
+
